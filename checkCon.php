17de977fd7f65823d37cd6a82b1e962187d4fff6
hhm4 <hhm4@njit.edu>
2015-11-15 21:01:39 -0500

    debug

--- checkCon.php
+++ checkCon.php
@@ -1,7 +1,10 @@
 <?php
 
 $config=parse_ini_file("/afs/cad/u/h/h/hhm4/mysql.ini",false,true);
-
+if (parse_ini_file("/afs/cad/u/h/h/hhm4/mysql.ini",false,true){
+	print 'hari';
+}
+echo $config;
 echo $config['host'];
 echo $config['username'];
 echo $config['password'];
