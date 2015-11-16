2c7e20ef1426442732453143762c3f2cec5b66b0
hhm4 <hhm4@njit.edu>
2015-11-16 14:09:46 -0800

    dd

--- checkCon.php
+++ checkCon.php
@@ -2,7 +2,7 @@
 
 $config=parse_ini_file("/afs/cad/u/h/h/hhm4/public_html/.mysql.ini",false,true);
 
-$con=mysql_connect($config['host'],$config['username'],$config['password']);
+global $con=mysql_connect($config['host'],$config['username'],$config['password']);
 if(!$con)
 {
 	print "Not connected";
