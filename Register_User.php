d62e50111ba96a1fcd93d559991b79383308f242
hhm4 <hhm4@njit.edu>
2015-11-16 14:22:27 -0800

    changed to old code

--- Register_User.php
+++ Register_User.php
@@ -1,32 +1,15 @@
 <?php
-
 $config=parse_ini_file("/afs/cad/u/h/h/hhm4/public_html/.mysql.ini",false,true);
 $con=mysql_connect($config['host'],$config['username'],$config['password']);
-$dbCon=mysql_select_db($config['database'], $con);
-sign_up();
-
-
-function sign_up()
+if(!$con)
 {
-	global $connect;
-	//$json_string = $_POST['userInfo'];
-	//$userInfo = json_decode($json_string);
-	$emailID = 'hi';
-	$username = 'hello';
-	$password = 'qwerty';
-	$result= mysql_query('CALL Register_NewUser($username,$emailID,$password)',$con);
-	
-	if($result>0)
-		
-		echo "Success";
-	}
-	else
-	{
-		"registered";
-	}
-	mysqli_close($connect);
-	print_r('Success');
+	print "Not connected";
 }
-
-
+$dbCon=mysql_select_db($config['database'], $con);
+print "connected".$dbCon;
+$sql=mysql_query('SELECT GroupName FROM CHATROOM_USERS', $con);
+while($row = mysql_fetch_assoc($sql))
+{
+echo $row['GroupName'];
+}
 ?>
