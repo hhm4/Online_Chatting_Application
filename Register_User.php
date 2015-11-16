f211669af576ca70886ad65f4b40ed7f59376384
hhm4 <hhm4@njit.edu>
2015-11-16 14:20:06 -0800

    select statement added

--- Register_User.php
+++ Register_User.php
@@ -2,6 +2,7 @@
 
 $config=parse_ini_file("/afs/cad/u/h/h/hhm4/public_html/.mysql.ini",false,true);
 $con=mysql_connect($config['host'],$config['username'],$config['password']);
+$dbCon=mysql_select_db($config['database'], $con);
 sign_up();
 
 
