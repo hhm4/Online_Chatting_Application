8a2d89f8a9f4d38d0109304651d285b5bd34600c
hhm4 <hhm4@njit.edu>
2015-11-16 13:55:22 -0800

    starting up

--- checkCon.php
+++ checkCon.php
@@ -7,13 +7,4 @@ if(!$con)
 {
 	print "Not connected";
 }
-$dbCon=mysql_select_db($config['database'], $con);
-print "connected".$dbCon;
-$sql=mysql_query('SELECT GroupName FROM CHATROOM_USERS', $con);
-
-while($row = mysql_fetch_assoc($sql))
-{
-echo $row['GroupName'];
-
-}
 ?>
