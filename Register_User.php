686b573759d7d1b02dddfbd9561f994743230938
hhm4 <hhm4@njit.edu>
2015-11-16 16:54:33 -0800

    ddd

--- Register_User.php
+++ Register_User.php
@@ -7,9 +7,9 @@ if(!$con)
 }
 $dbCon=mysql_select_db($config['database'], $con);
 print "connected".$dbCon;
-$sql=mysql_query('SELECT GroupName FROM CHATROOM_USERS', $con);
+$sql=mysql_query('CALL TESTING()', $con);
 while($row = mysql_fetch_assoc($sql))
 {
-echo $row['GroupName'];
+echo $row['UserName'];
 }
 ?>
