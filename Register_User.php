e81dcef4249c611fcc9ecad2cd4dabc83d9f6626
hhm4 <hhm4@njit.edu>
2015-11-16 18:47:33 -0800

    fgfdgf

--- Register_User.php
+++ Register_User.php
@@ -7,15 +7,14 @@ if(!$con)
 	print "Not connected";
 }
 $dbCon=mysql_select_db($config['database'], $con);
-$json_string = $_POST['userInfo'];
+/* $json_string = $_POST['userInfo'];
 	echo $json_string;
-	$userInfo = json_decode($json_string);
-	$emailID = $userInfo->emailID;
-	$username = $userInfo->username;
-	$password = $userInfo->Password;
-	echo "\n"."emailID:". $emailID ."\n". "username:". $username ."\n". "password:" . $password ;
-
-	$query = " Insert into USERS(UserName, EmailId, User_Password) values('$username', '$emailID', '$password')";
+	$userInfo = json_decode($json_string); */
+	$emailID = 'shoot the kuruvi';
+	$username = 'shoot the kuruviname';
+	$password = '123';
+	
+	$query = " Insert into hhm4.USERS(UserName, EmailId, User_Password) values('$username', '$emailID', '$password')";
 	
 	
 	$count=mysql_query($connect,$query) or die(mysql_error($connect)) or die(mysql_error());
