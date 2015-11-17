ef6e5effb836d96128a771e632be2a9454c84780
hhm4 <hhm4@njit.edu>
2015-11-17 15:14:39 -0800

    knkknknk

--- newuser.php
+++ newuser.php
@@ -1,21 +1,3 @@
 <?php
-$config=parse_ini_file("/afs/cad/u/h/h/hhm4/public_html/.mysql.ini",false,true);
-$con=mysql_connect($config['host'],$config['username'],$config['password']);
-if(!$con)
-{
-	print "Not connected";
-}
-$dbCon=mysql_select_db($config['database'], $con);
-$json_string = $_POST['userInfo'];
-$userInfo = json_decode($json_string);
-$username = $userInfo->username;
-$password = $userInfo->Password;
-$email1=$userInfo->Email;
-
-$sql=mysql_query("CALL Register_NewUser('$username','$email1','$password');", $con);
-while($row = mysql_fetch_assoc($sql))
-{
-echo $row['UserId'];
-}
-
+echo"hi how are u";
 ?>
