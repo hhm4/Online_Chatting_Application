55dd6f183d5c8637e10935597fef5d5bed991fec
hhm4 <hhm4@njit.edu>
2015-11-16 21:18:41 -0800

    dddd

--- Register_User.php
+++ Register_User.php
@@ -6,10 +6,11 @@ if(!$con)
 	print "Not connected";
 }
 $dbCon=mysql_select_db($config['database'], $con);
-// $json_string = $_POST['userInfo'];
-// $userInfo = json_decode($json_string);
-// $username = $userInfo->username;
-// $password = $userInfo->Password;
-// $email1=$userInfo->Email;
+$json_string = $_POST['userInfo'];
+$userInfo = json_decode($json_string);
+$username = $userInfo->username;
+$password = $userInfo->Password;
+$email1=$userInfo->Email;
+echo '$username';
 $sql=mysql_query('Insert into hhm4.USERS(UserName,EmailId,User_Password) values ("dddd","dddd","dddd");', $con);
 ?>
