3856d99fb73d12ded8ea65f3458e35042af1436e
hhm4 <hhm4@njit.edu>
2015-11-16 17:56:25 -0800

    aas

--- Register_User.php
+++ Register_User.php
@@ -10,7 +10,11 @@ $email='gggg';
 $pwd='123';
 $dbCon=mysql_select_db($config['database'], $con);
 print "connected".$dbCon;
-$sql=mysql_query('CALL TESTING($usernam,$email,$pwd)', $con);
+if(!(mysql_query('CALL TESTING($usernam,$email,$pwd)', $con))
+{
+	echo "Prepare failed: (" . $mysql_errno . ") " . $mysql_error;
+	
+};
 while($row = mysql_fetch_assoc($sql))
 {
 echo $row['GroupName'];
