d9bbf3f29cad27c8aeefb0676d110db2bef9903a
hhm4 <hhm4@njit.edu>
2015-11-16 17:59:45 -0800

    jkkjj

--- Register_User.php
+++ Register_User.php
@@ -12,7 +12,7 @@ $dbCon=mysql_select_db($config['database'], $con);
 print "connected".$dbCon;
 if(!(mysql_query('CALL TESTING($usernam,$email,$pwd)', $con))
 {
-	echo "Prepare failed: (" . $mysql_errno . ") " . $mysql_error;
+	echo "Prepare failed: (" . $mysql_errno() . ") " . $mysql_error();
 	
 };
 while($row = mysql_fetch_assoc($sql))
