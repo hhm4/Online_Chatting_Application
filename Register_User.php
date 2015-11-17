9233b3cb03db3677dfaa825da3cc0a431825c4fe
hhm4 <hhm4@njit.edu>
2015-11-16 17:36:25 -0800

    sss

--- Register_User.php
+++ Register_User.php
@@ -1,13 +1,18 @@
 <?php
 $config=parse_ini_file("/afs/cad/u/h/h/hhm4/public_html/.mysql.ini",false,true);
-$con=mysql_connect($config['host'],$config['username'],$config['password']);
+$con=mysqli_connect($config['host'],$config['username'],$config['password']);
 if(!$con)
 {
 	print "Not connected";
 }
-$dbCon=mysql_select_db($config['database'], $con);
+$usernam= 'rrr';
+$email='gggg';
+$pwd='123';
+$dbCon=mysqli_select_db($config['database'], $con);
 print "connected".$dbCon;
-$sql=mysql_query('CALL TESTING()', $con);
+$sql=mysqli_prepare('CALL Register_User(?,?,?)', $con);
+mysqli_stmt_bind_param($usernam,$email,$pwd);
+mysqli_stmt_execute($sql);
 while($row = mysql_fetch_assoc($sql))
 {
 echo $row['GroupName'];
