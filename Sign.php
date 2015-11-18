1064c2a5c0e056ae4289a2af3d3e92b26343e013
hhm4 <hhm4@njit.edu>
2015-11-18 16:33:19 -0500

    sign

--- Sign.php
+++ Sign.php
@@ -2,8 +2,8 @@
 
 if( $_POST["name"] || $_POST["email"] || $_POST["contact"])
 {
-echo "Welcome: ". $_POST['name']. "<br />";
-echo "Your Email is: ". $_POST["email"]. "<br />";
+echo "Welcome: ". $_POST['name'];
+echo "Your Email is: ". $_POST["email"];
 echo "Your Mobile No. is: ". $_POST["contact"];
 }
 
