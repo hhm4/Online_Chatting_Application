3a08580a83f1e9c7b36e16faaec197c6d0604a18
hhm4 <hhm4@njit.edu>
2015-11-19 08:22:09 -0800

    kadsk

--- Verification.php
+++ Verification.php
@@ -10,23 +10,19 @@ $dbCon=mysql_select_db($config['database'], $con);
 $token=$_POST['Token'];
 $email=$_POST['EmailId'];
 $query="Select * From UNVERIFIED_USERS where Token='$token' AND EmailId='$email'";
-echo $query;
-# $query = sprintf("Select * From UNVERIFIED_USERS where Token=18714 AND EmailId='%s'",
-// # mysql_real_escape_string($email));
-// $query = sprintf("Select * From UNVERIFIED_USERS where Token='%d'",18714);
-// $registration=mysql_query($query,$con);
-// #$registration=mysql_query("Select count(*) From UNVERIFIED_USERS where TOKEN=' ". $vc. " ' AND (EmailId=' ".$email. " ')",$con);
-// #$registration=mysql_query("CALL FetchUnverifiedUsers('$email','$vc')",$con);
-// echo '     ';
-// echo mysql_num_rows($registration);
-// if (mysql_num_rows($registration)){
+
+ $registration=mysql_query($query,$con);
+
+ $count=mysql_num_rows($registration);
+ echo $count;
+ if ($count>0){
 	// $row = mysql_fetch_array($registration, MYSQL_ASSOC);
 	// $name=$row['UserName'];
 	// $em=$row['User_Password'];
 	// $sql=mysql_query("Insert into USERS(UserName,EmailId,User_Password) values('{$name}','{$email}','{$em}')", $con);
 	// $delete=mysql_query("Delete FROM UNVERIFIED_USERS where EmailId='".$email."'",$con);
 	// $response=array("Result"=>1);
-// }
+ }
 // else{
 	// $response=array("Result"=>0,"Type"=>gettype($vc));
 // }
