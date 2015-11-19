2bad565fcb0d8ad5df62538118cec226b5019d23
hhm4 <hhm4@njit.edu>
2015-11-19 03:11:23 -0500

    code identified

--- SignUp.php
+++ SignUp.php
@@ -12,22 +12,22 @@ $username=$_POST['UserName'];
 $password=$_POST['Password'];
 $email=$_POST['EmailId'];
 #$sql=mysql_query("Insert into USERS(UserName,EmailId,User_Password) values('{$username}','{$email}','{$password}')", $con);
-$existingUser=("select count(*) from USERS where EmailId='$email'", $con);
-// if($existingUser==0){
-// 	$id= mysql_query("SELECT FLOOR(RAND()*40000)+10000", $con);
+$existingUser=mysql_query("select count(*) from USERS where EmailId='$email'", $con);
+if($existingUser==0){
+	$id= mysql_query("SELECT FLOOR(RAND()*40000)+10000", $con);
 
-// $verification=mysql_query("Insert into UNVERIFIED_USERS (UserName,EmailId,User_Password,Token) values ('{$username}','{$email}','{$password}',$id)", $con);
+$verification=mysql_query("Insert into UNVERIFIED_USERS (UserName,EmailId,User_Password,Token) values ('{$username}','{$email}','{$password}',$id)", $con);
 	
-// 	if($verification){
-// 		$response=array("Result"=>0);
-// 	}
-// }
-// else{
-// 	$response=array("Result"=>"1");
-// }
-// $encoded = json_encode($response);
-// header('Content-type: application/json');
-// echo $encoded;
+	if($verification){
+		$response=array("Result"=>0);
+	}
+}
+else{
+	$response=array("Result"=>"1");
+}
+$encoded = json_encode($response);
+header('Content-type: application/json');
+echo $encoded;
 
 mysql_close();
 ?>
