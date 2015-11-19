c679240ee10451993b84efac634602ddcccf8961
hhm4 <hhm4@njit.edu>
2015-11-19 07:32:15 -0800

    bvvh

--- Verification.php
+++ Verification.php
@@ -9,7 +9,9 @@ if(!$con)
 }
 $dbCon=mysql_select_db($config['database'], $con);
 $email=$_POST['Token'];
+$email1=$_POST['EmailId'];
 echo $email;
+echo $email1;
 
 // #$query = sprintf("Select * From UNVERIFIED_USERS where Token=18714 AND EmailId='%s'",
 // # mysql_real_escape_string($email));
