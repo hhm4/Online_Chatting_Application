d53b9b28cfbf781d954169e332bf9d73a95f86ab
hhm4 <hhm4@njit.edu>
2015-11-19 03:21:00 -0500

    fsdf

--- SignUp.php
+++ SignUp.php
@@ -12,7 +12,7 @@ $username=$_POST['UserName'];
 $password=$_POST['Password'];
 $email=$_POST['EmailId'];
 #$sql=mysql_query("Insert into USERS(UserName,EmailId,User_Password) values('{$username}','{$email}','{$password}')", $con);
-$existingUser=mysql_query("select * from USERS where EmailId='$email'", $con);
+$existingUser=mysql_query("select * from USERS where EmailId='{$email}'", $con);
 echo $existingUser;
 if($existingUser){
 	$id= mysql_query("SELECT FLOOR(RAND()*40000)+10000", $con);
