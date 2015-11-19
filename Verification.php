5a16a377d42991356a31c2f195f53f77b942164f
hhm4 <hhm4@njit.edu>
2015-11-19 06:25:32 -0800

    cvcbvbvb

--- Verification.php
+++ Verification.php
@@ -11,8 +11,8 @@ $dbCon=mysql_select_db($config['database'], $con);
 $VerificationCode=$_POST['VerificationCode'];
 $vc=intval($VerificationCode);
 $email=$_POST['EmailId'];
-$query = sprintf("Select * From UNVERIFIED_USERS where Token='%d' AND EmailId='%s'",
-    mysql_real_escape_string($VerificationCode),mysql_real_escape_string($email));
+$query = sprintf("Select * From UNVERIFIED_USERS where Token='$vc' AND EmailId='%s'",
+    mysql_real_escape_string($email));
 
 $registration=mysql_query($query,$con);
 #$registration=mysql_query("Select count(*) From UNVERIFIED_USERS where TOKEN=' ". $vc. " ' AND (EmailId=' ".$email. " ')",$con);
