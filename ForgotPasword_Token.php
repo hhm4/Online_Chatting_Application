87e118dc762f04068bdd99afa7d95404d559be95
hhm4 <hhm4@njit.edu>
2015-11-19 13:02:02 -0800

    Forgot Password Start

---
+++ ForgotPasword_Token.php
@@ -0,0 +1,12 @@
+<?php
+$config=parse_ini_file("/afs/cad/u/h/h/hhm4/public_html/.mysql.ini",false,true);
+$con=mysql_connect($config['host'],$config['username'],$config['password']);
+if(!$con)
+{
+	print "Not connected";
+}
+$dbCon=mysql_select_db($config['database'], $con);
+$email=$_POST['EmailId'];
+echo $email;
+
+?>
