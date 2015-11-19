2641eacfd8e87050d7ae1d78d5c899b811b4acf9
hhm4 <hhm4@njit.edu>
2015-11-19 08:37:07 -0800

    lksjdlfkdsjf

--- Verification.php
+++ Verification.php
@@ -8,6 +8,7 @@ if(!$con)
 }
 $dbCon=mysql_select_db($config['database'], $con);
 $token=$_POST['Token'];
+$token=(int)$token;
 $email=$_POST['EmailId'];
 $query="Select * From UNVERIFIED_USERS where Token='$token' AND EmailId='$email'";
 
