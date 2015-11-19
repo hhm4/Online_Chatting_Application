8cc4c463063be2c67fedf7e422983824bc321d8f
hhm4 <hhm4@njit.edu>
2015-11-19 17:17:13 -0500

    kjhkj

--- SignIn.php
+++ SignIn.php
@@ -7,7 +7,7 @@ $con=mysql_connect($config['host'],$config['username'],$config['password']);
 $dbcon=mysql_select_db($config['database'],$con);
 $email=$_POST['EmailId'];
 $password=$_POST['Passsword'];
-$authentication=mysql_query("select * from USERS where (EmailId='{$email}' AND Passsword='{$password}')",$con);
+$authentication=mysql_query("select * from USERS where EmailId='".$email."' AND Passsword='".$password."'",$con);
 $count=mysql_num_rows($authentication);
 echo $count;
 mysql_close();
