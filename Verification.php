6a99f44fcbad249ac0462bac36edbb8417e701c0
hhm4 <hhm4@njit.edu>
2015-11-19 13:15:35 -0500

    lksjdflks

--- Verification.php
+++ Verification.php
@@ -8,10 +8,10 @@ if(!$con)
 	print "Not connected";
 }
 $dbCon=mysql_select_db($config['database'], $con);
-//$VerificationCode=(int)$_POST['VerificationCode'];
-//$email=$_POST['EmailId'];
-$email="ysudhdj";
-$registration=mysql_query("Select * From UNVERIFIED_USERS where TOKEN=16634 AND EmailId='".$email."' ",$con);
+$VerificationCode=(int)$_POST['VerificationCode'];
+$email=$_POST['EmailId'];
+//$email="ysudhdj";
+$registration=mysql_query("Select * From UNVERIFIED_USERS where TOKEN=".$VerificationCode." AND EmailId='".$email."' ",$con);
 if (mysql_num_rows($registration)!=0){
 	$row = mysql_fetch_array($registration, MYSQL_ASSOC);
 	$name=$row['UserName'];
