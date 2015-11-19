7a439d2154997bd44b3b4172511dd2e59e7dd770
hhm4 <hhm4@njit.edu>
2015-11-19 06:40:37 -0800

    sadsdsd
    ddsd

---
+++ SignIn_validation.php
@@ -0,0 +1,14 @@
+<?php
+$config=parse_ini_file("/afs/cad/u/h/h/hhm4/public_html/.mysql.ini",false,true);
+$con=mysql_connect($config['host'],$config['username'],$config['password']);
+if(!$con)
+{
+	print "Not connected";
+}
+$dbCon=mysql_select_db($config['database'], $con);
+$json_string = $_POST['userInfo'];
+$userInfo = json_decode($json_string);
+$username = $userInfo->username;
+$password = $userInfo->Password;
+
+?>
