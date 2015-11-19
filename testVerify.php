38f24e2d8427490ae3e2a4e84e3e6419c7109f1c
mdlkumaran <mdlkumaran@gmail.com>
2015-11-19 08:52:26 -0500

    sdssd

--- testVerify.php
+++ testVerify.php
@@ -5,10 +5,10 @@ $subject = "this camr from mother";
 $name = 'Kumaran';
 $email = 'mdlkumaran@gmail.com';
 $message= "Hi I am gud";
-
+$header  '$email';
 $config=parse_ini_file("/afs/cad/u/h/h/hhm4/public_html/.mysql.ini",false,true);
 $con=mysql_connect($config['host'],$config['username'],$config['password']);
-mailto($to, $subject, $message, $email);
+mailto($to, $subject, $message, $header);
 if(!$con)
 {
 	print "Not connected";
