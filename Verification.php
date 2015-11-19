11b01f82f73787b549a4dad07124183b5347e1d4
hhm4 <hhm4@njit.edu>
2015-11-19 05:09:52 -0800

    xzxz

--- Verification.php
+++ Verification.php
@@ -11,11 +11,11 @@ $dbCon=mysql_select_db($config['database'], $con);
 $VerificationCode=$_POST['VerificationCode'];
 $vc=intval($VerificationCode);
 $email=$_POST['EmailId'];
-$registration=mysql_query("Select count(*) From UNVERIFIED_USERS where Token='{$VerificationCode} AND Emailid= '".$email."'",$con);
+$registration=mysql_query("Select * From UNVERIFIED_USERS where Token='{$VerificationCode}' AND Emailid= '".$email."'",$con);
 #$registration=mysql_query("Select count(*) From UNVERIFIED_USERS where TOKEN=' ". $vc. " ' AND (EmailId=' ".$email. " ')",$con);
 #$registration=mysql_query("CALL FetchUnverifiedUsers('$email','$vc')",$con);
 echo $registration;
-if ($registration=1){
+if (mysql_num_rows($registration)){
 	$row = mysql_fetch_array($registration, MYSQL_ASSOC);
 	$name=$row['UserName'];
 	$em=$row['User_Password'];
