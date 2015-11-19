3adafc4210dda534eebc6629c23fd7e01f871c9a
hhm4 <hhm4@njit.edu>
2015-11-19 02:10:18 -0500

    adding sat

--- SignUp.php
+++ SignUp.php
@@ -11,15 +11,15 @@ $dbCon=mysql_select_db($config['database'], $con);
 $username=$_POST['UserName'];
 $password=$_POST['Password'];
 $email=$_POST['EmailId'];
-$sql=mysql_query("Insert into USERS(UserName,EmailId,User_Password) values('{$username}','{$email}','{$password}')", $con);
-#$existingUser=("select count(*) from USERS where EmailId='$email'");
-#if($existingUser==0){
-##	$sql=mysql_query("Insert into USERS(UserName,EmailId,User_Password) values('{$username}','{$password}','{$email}')", $con);
-#	$response=array("Result"=>1);
-#}
-##else{
-	$response=array("Result"=>"0");
-#}
+#$sql=mysql_query("Insert into USERS(UserName,EmailId,User_Password) values('{$username}','{$email}','{$password}')", $con);
+$existingUser=("select count(*) from USERS where EmailId='$email'");
+if($existingUser==0){
+#	$sql=mysql_query("Insert into USERS(UserName,EmailId,User_Password) values('{$username}','{$password}','{$email}')", $con);
+	$response=array("Result"=>0);
+}
+else{
+	$response=array("Result"=>"1");
+}
 $encoded = json_encode($response);
 header('Content-type: application/json');
 echo $encoded;
