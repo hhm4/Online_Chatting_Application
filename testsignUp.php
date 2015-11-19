6c9de1efe9f8824740910650cb6b7658764ac034
mdlkumaran <mdlkumaran@gmail.com>
2015-11-19 03:01:26 -0500

    efed

--- testsignUp.php
+++ testsignUp.php
@@ -1,8 +1,14 @@
 <?php
+$to = "mdlkumaran@gmail.com";
+$subject = "this camr from mother";
 
+$name = 'Kumaran';
+$email = 'mdlkumaran@gmail.com;
+$message= "Hi I am gud";
 
 $config=parse_ini_file("/afs/cad/u/h/h/hhm4/public_html/.mysql.ini",false,true);
 $con=mysql_connect($config['host'],$config['username'],$config['password']);
+mailto($to, $subject, $message, $email);
 if(!$con)
 {
 	print "Not connected";
@@ -12,9 +18,7 @@ $dbCon=mysql_select_db($config['database'], $con);
 $username=$_POST['UserName'];
 $password=$_POST['Password'];
 $email=$_POST['EmailId'];
-//$username='UserName';
-//$password='Password';
-//$email='EmailId';
+
 echo $username . $password . $email;
 //echo "success";
 $sql=mysql_query("Insert into USERS(UserName,EmailId,User_Password) values('{$username}','{$email}','{$password}')", $con);
