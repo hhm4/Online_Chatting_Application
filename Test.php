0fa4f04b1bb49f38bb78babbfcfbb3c10b9d30f0
hhm4 <hhm4@njit.edu>
2015-11-19 04:27:09 -0800

    Testing procedure call

--- Test.php
+++ Test.php
@@ -1,9 +1,14 @@
 <?php
-$json_string = $_POST['userInfo'];
-$userInfo = json_decode($json_string);
-$username = $userInfo->username;
-$password = $userInfo->Password;
-$email1=$userInfo->Email;
-echo $email1;
-echo "hi";
+$config=parse_ini_file("/afs/cad/u/h/h/hhm4/public_html/.mysql.ini",false,true);
+$con=mysql_connect($config['host'],$config['username'],$config['password']);
+if(!$con)
+{
+	print "Not connected";
+}
+$dbCon=mysql_select_db($config['database'], $con);
+
+$existingUser=mysql_query("CALL TESTING()", $con);
+
+echo mysql_num_rows($existingUser);
+
 ?>
