9c1b1613863f4a46fb16a81c248f540e363931cb
mdlkumaran <mdlkumaran@gmail.com>
2015-11-19 02:35:16 -0500

    jghj

--- testsignUp.php
+++ testsignUp.php
@@ -9,12 +9,15 @@ if(!$con)
 }
 print_r($_POST);
 $dbCon=mysql_select_db($config['database'], $con);
-$username=$_POST['UserName'];
-$password=$_POST['Password'];
-$email=$_POST['EmailId'];
-//echo $username . $password . $email;
+//$username=$_POST['UserName'];
+//$password=$_POST['Password'];
+//$email=$_POST['EmailId'];
+$username='UserName';
+$password='Password';
+$email='EmailId';
+echo $username . $password . $email;
 //echo "success";
-$sql=mysql_query("Insert into USERS(UserName,EmailId,User_Password) values('{$username}','{$password}','{$email}')", $con);
+$sql=mysql_query("Insert into USERS(UserName,EmailId,User_Password) values('{$username}','{$email}','{$password}')", $con);
 echo "sql:".$sql;
 if($sql)
 	{
