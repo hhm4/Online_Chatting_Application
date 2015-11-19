52f9ced7a3c185d1224a0c113719b85a9d7123d9
hhm4 <hhm4@njit.edu>
2015-11-19 16:44:08 -0500

    lkasdjf

--- SignIn.php
+++ SignIn.php
@@ -1,1 +1,14 @@
-SignIn.php
+<?php
+
+
+$config=parse_ini_file("/afs/cad/u/h/h/hhm4/public_html/.mysql.ini",false,true);
+$con=mysql_connect($config['host'],$config['username'],$config['password']);
+
+$dbcon=mysql_select_db($config['database'],$con);
+$email=$_POST['EmailId'];
+$password=$_POST['Passsword'];
+$authentication=mysql_query("select * from USERS where EmailId='{$email}' AND Passsword='{$password}'",$con);
+echo $authentication;
+mysql_close();
+
+?>
