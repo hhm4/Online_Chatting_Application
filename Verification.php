f1b498042318f21ca0a65df0e38331e6941d9a22
hhm4 <hhm4@njit.edu>
2015-11-19 04:49:54 -0800

    asasa

--- Verification.php
+++ Verification.php
@@ -11,7 +11,7 @@ $dbCon=mysql_select_db($config['database'], $con);
 $VerificationCode=$_POST['VerificationCode'];
 $vc=intval($VerificationCode);
 $email=$_POST['EmailId'];
-$registration=mysql_query("Select count(*) From UNVERIFIED_USERS where EmailId=' ".$email. " '",$con);
+$registration=mysql_query("Select count(*) From UNVERIFIED_USERS where Token=' ".$VerificationCode. " '",$con);
 #$registration=mysql_query("Select count(*) From UNVERIFIED_USERS where TOKEN=' ". $vc. " ' AND (EmailId=' ".$email. " ')",$con);
 #$registration=mysql_query("CALL FetchUnverifiedUsers('$email','$vc')",$con);
 echo $registration;
@@ -19,7 +19,7 @@ if ($registration=1){
 	$row = mysql_fetch_array($registration, MYSQL_ASSOC);
 	$name=$row['UserName'];
 	$em=$row['User_Password'];
-	$sql=mysql_query("Insert into USERS(UserName,EmailId,User_Password) values('$name','$email','$em')", $con);
+	$sql=mysql_query("Insert into USERS(UserName,EmailId,User_Password) values('{$name}','{$email}','{$em}')", $con);
 	$delete=mysql_query("Delete FROM UNVERIFIED_USERS where EmailId='".$email."'",$con);
 	$response=array("Result"=>1);
 }
