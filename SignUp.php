b5abba6e1ea18566bb50699050abdec6f2f4c416
hhm4 <hhm4@njit.edu>
2015-11-20 15:39:16 -0800

    cmcvmcnmcvn

--- SignUp.php
+++ SignUp.php
@@ -9,17 +9,30 @@ $dbCon=mysql_select_db($config['database'], $con);
 $username=$_POST['UserName'];
 $password=$_POST['Password'];
 $email=$_POST['EmailId'];
-$query = sprintf("SELECT count(*) from USERS where EmailId='%s'",
-    mysql_real_escape_string($email));
-
-$existingUser=mysql_query($query, $con);
-if($existingUser>0)
+$query="select * from USERS where EmailId='$email'";
+$authentication=mysql_query($query,$con);
+if(mysql_num_rows($authentication)>0)
 {
 	$id= rand(10000,20000);
+	$verify=mysql_query("select * from UNVERIFIED_USERS where EmailId='$email'",$con)
+	if(mysql_num_rows($verify)>0)
+	{
+		$verification=mysql_query("Update UNVERIFIED_USERS set Token ='$id' where EmailId='$email'");
+	}
+    else
+	{
 	$verification=mysql_query("Insert into UNVERIFIED_USERS (UserName,EmailId,User_Password,Token) values ('{$username}','{$email}','{$password}',$id)", $con);
-	if($verification){
-			mail($email, "Online Chat verification", "Verification Code :".$id);
-			$response=array("Result"=>0);
+	}
+	
+            $message = "
+<html><head><title></title></head><body><p> Sign Up Mail</p><table><tr><th>Please enter the  verification code manually in the application</th><th>";
+$message.=$id;
+$message.="</th></tr><tr></tr></table></body></html>";
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$headers .= 'From: <Online chat Team>' . "\r\n";
+			mail($email, "Password Reset", $message,$headers);
+			$response=array("Result"=>0);	
 	}
 }
 else{
@@ -29,6 +42,5 @@ $encoded = json_encode($response);
 header('Content-type: application/json');
 echo $encoded;
 mysql_close();
-#$sql=mysql_query("Insert into USERS(UserName,EmailId,User_Password) values('{$username}','{$email}','{$password}')", $con);
 
 ?>
