67c53264a2fe73dc73aaa79750648673661b3f76
mdlkumaran <mdlkumaran@gmail.com>
2015-11-19 20:40:10 -0500

    dfsd

--- outparam.php
+++ outparam.php
@@ -1,8 +1,9 @@
 <?php
 echo "hi";
-//$config=parse_ini_file("/afs/cad/u/h/h/hhm4/public_html/.mysql.ini",false,true);
-//$mysqli=mysql_connect($config['host'],$config['username'],$config['password']);
-//$dbCon=mysql_select_db($config['database'], $con);
+$config=parse_ini_file("/afs/cad/u/h/h/hhm4/public_html/.mysql.ini",false,true);
+$mysqli=mysql_connect($config['host'],$config['username'],$config['password']);
+$dbCon=mysql_select_db($config['database'], $con);
+echo "hiii";
 //$username = "sheik";
 //echo $username;
 /*$mysqli = mysqli_connect();
