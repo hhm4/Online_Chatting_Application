920c0e7b17a3fed7947abd136e3ebf0b6891d854
mdlkumaran <mdlkumaran@gmail.com>
2015-11-19 20:35:47 -0500

    dfddf

--- outparam.php
+++ outparam.php
@@ -1,17 +1,17 @@
 <?php
-
-$config=parse_ini_file("/afs/cad/u/h/h/hhm4/public_html/.mysql.ini",false,true);
-$mysqli=mysql_connect($config['host'],$config['username'],$config['password']);
-$dbCon=mysql_select_db($config['database'], $con);
-$username = "sheik";
-
-$mysqli = mysqli_connect();
+echo "hi";
+//$config=parse_ini_file("/afs/cad/u/h/h/hhm4/public_html/.mysql.ini",false,true);
+//$mysqli=mysql_connect($config['host'],$config['username'],$config['password']);
+//$dbCon=mysql_select_db($config['database'], $con);
+//$username = "sheik";
+//echo $username;
+/*$mysqli = mysqli_connect();
 
 $call = mysqli_prepare($mysqli, 'CALL Test_OutProcedure(?, @result)');
 mysqli_stmt_bind_param($call, 's', $username);
 mysqli_stmt_execute($call);
 mysqli_stmt_bind_result($call, $result);
 mysqli_stmt_fetch($call);
-echo $result;
+echo $result;*/
            
 ?>
