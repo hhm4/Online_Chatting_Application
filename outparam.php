8689f8ec327bfe8e888ec089f06a3b1f41e9c66d
mdlkumaran <mdlkumaran@gmail.com>
2015-11-19 20:42:11 -0500

    erew

--- outparam.php
+++ outparam.php
@@ -4,15 +4,15 @@ $config=parse_ini_file("/afs/cad/u/h/h/hhm4/public_html/.mysql.ini",false,true);
 $mysqli=mysql_connect($config['host'],$config['username'],$config['password']);
 $dbCon=mysql_select_db($config['database'], $con);
 echo "hiii";
-//$username = "sheik";
-//echo $username;
-/*$mysqli = mysqli_connect();
+$username = "sheik";
+echo $username;
+$mysqli = mysqli_connect();
 
 $call = mysqli_prepare($mysqli, 'CALL Test_OutProcedure(?, @result)');
 mysqli_stmt_bind_param($call, 's', $username);
 mysqli_stmt_execute($call);
 mysqli_stmt_bind_result($call, $result);
 mysqli_stmt_fetch($call);
-echo $result;*/
+echo $result;
            
 ?>
