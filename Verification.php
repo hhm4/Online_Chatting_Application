7268af4017d3b45452d771fd12fc528717a0062e
hhm4 <hhm4@njit.edu>
2015-11-20 18:07:35 -0500

    klsdjlfk

--- Verification.php
+++ Verification.php
@@ -24,11 +24,9 @@ $query="Select * From UNVERIFIED_USERS where Token=$token AND EmailId='$email'";
 	$row = mysql_fetch_array($registration, MYSQL_ASSOC);
 	 $name=$row['UserName'];
 	 $pass=$row['User_Password'];
-	 
 	 $sql=mysql_query("Insert into USERS(UserName,EmailId,User_Password) values('{$name}','{$email}','{$pass}')", $con);
-	 
-	 $response=array("Result"=>1,"query"=>$query);
 	 $delete=mysql_query("Delete FROM UNVERIFIED_USERS where EmailId='$email'",$con);
+	 $response=array("Result"=>1,"query"=>$query);
  }
 #$query2="Select * From USERS where Token=$token AND EmailId='$email'";
 #$registration2=mysql_query($query2,$con);
