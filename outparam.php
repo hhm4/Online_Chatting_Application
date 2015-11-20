4ad1e37276459530fb1c663357d5b8e97f1a2a94
mdlkumaran <mdlkumaran@gmail.com>
2015-11-19 20:09:38 -0500

    dafad

--- outparam.php
+++ outparam.php
@@ -5,14 +5,16 @@ $mysqli=mysql_connect($config['host'],$config['username'],$config['password']);
 
 $username = "sheik";
 bool $ result = TRUE;
-
+echo "1result:".$result;
 $mysqli = mysqli_connect();
-
+echo "2result:".$result;
 $call = mysqli_prepare($mysqli, 'CALL Test_OutProcedure(?)');
+echo "3result:".$result;
 mysqli_stmt_bind_param($call, 's', $username);
 mysqli_stmt_execute($call);
+echo "4result:".$result;
 mysqli_stmt_bind_result($call, $result);
 mysqli_stmt_fetch($call);
-echo "result:".$result;
+echo "5result:".$result;
            
 />
