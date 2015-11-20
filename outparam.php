e71336ba1d89484c1f3d8a7d802b3e8f1bf51c01
mdlkumaran <mdlkumaran@gmail.com>
2015-11-19 20:01:10 -0500

    dsds

--- outparam.php
+++ outparam.php
@@ -8,7 +8,7 @@ $username = "sheik";
 
 $mysqli = mysqli_connect();
 
-$call = mysqli_prepare($mysqli, 'CALL Test_OutProcedure(?, @result)');
+$call = mysqli_prepare($mysqli, 'CALL Test_OutProcedure(?)');
 mysqli_stmt_bind_param($call, 's', $username);
 mysqli_stmt_execute($call);
 mysqli_stmt_bind_result($call, $result);
