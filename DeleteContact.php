77be4ed50b4596a9ac7d14f979dcd5239b7900ab
hhm4 <hhm4@njit.edu>
2015-11-20 07:22:38 -0800

    ddd

---
+++ DeleteContact.php
@@ -0,0 +1,58 @@
+<?php
+
+$config=parse_ini_file("/afs/cad/u/h/h/hhm4/public_html/.mysql.ini",false,true);
+$con=mysql_connect($config['host'],$config['username'],$config['password']);
+if(!$con)
+{
+	print "Not connected";
+}
+$dbCon=mysql_select_db($config['database'], $con);
+
+$email=$_POST['EmailId'];
+$ContactsName=$_POST['ContactName'];
+$query="Select * From USERS where EmailId='$email'";
+$registration=mysql_query($query,$con);
+$count=mysql_num_rows($registration);
+if($count!=0)
+{
+$row = mysql_fetch_array($registration, MYSQL_ASSOC);
+$userid=$row['UserId'];
+$userstatus=$row['UserStatus'];
+$contactsquery="Select * From CONTACTS where Contacts_UserId='$userid'";
+$contactscheck=mysql_query($contactsquery,$con);
+$contactscount=mysql_num_rows($contactscheck);
+  if($contactscount==0)
+   {
+	$sql=mysql_query("Insert into CONTACTS(Contacts_UserId,Contact_UserName,Contact_Status) values('{$userid}','{$ContactsName}','{$userstatus}')", $con);
+	$response=array("Result"=>0);
+   }
+   
+   else
+   {
+	$response=array("Result"=>1);
+   }
+	
+}
+
+else
+ {
+	 $message = "
+<html><head><title>HTML email</title></head><body><p> Join our Exciting online Chat Application</p><table><tr><th>Please click the below link to join our exciting Chat application</th><th>www.onlinechat.com</th></tr><tr></tr>
+</table></body></html>
+";
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// More headers
+$headers .= 'From: <Online chat Team>' . "\r\n";
+
+
+	 mail($email, "Join Online Chat",$message,$headers);
+	 $response=array("Result"=>2);
+	 
+ }
+$encoded = json_encode($response);
+header('Content-type: application/json');
+echo $encoded;
+mysql_close();
+?>
--- ForgotPassword_Token.php
+++ ForgotPassword_Token.php
@@ -8,16 +8,31 @@ if(!$con)
 $dbCon=mysql_select_db($config['database'], $con);
 $email=$_POST['EmailId'];
 $query="select * from USERS where EmailId='$email'";
-$query1="select * from Token_Verification where EmailId='$email'";
 $authentication=mysql_query($query,$con);
-$emailauthentication=mysql_query($query1,$con);
 $count=mysql_num_rows($authentication);
-$count1=mysql_num_rows($emailauthentication);
-if($count>0 && $count1 ==0)
+if($count>0)
 {
+$query1="select * from Token_Verification where EmailId='$email'";
+$emailauthentication=mysql_query($query1,$con);
+$count1=mysql_num_rows($emailauthentication);
 $id= rand(1000000,2000000);
-$verification=mysql_query("Insert into Token_Verification(EmailId,Token) values ('{$email}',$id)", $con);
- $message = "
+if($count1>0)
+{
+	$verification=mysql_query("Update Token_Verification SET Token='$id' where EmailId='$email'", $con);
+	$message = "
+<html><head><title></title></head><body><p> Password Reset Token Resent</p><table><tr><th>Reset your Password by entering the new verification code manually in the application</th><th>";
+$message.=$id;
+$message.="</th></tr><tr></tr></table></body></html>";
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$headers .= 'From: <Online chat Team>' . "\r\n";
+			mail($email, "Password Reset", $message,$headers);
+			$response=array("Result"=>0);
+}
+else
+{
+   $verification=mysql_query("Insert into Token_Verification(EmailId,Token) values ('{$email}',$id)", $con);
+   $message = "
 <html><head><title></title></head><body><p> Password Reset Token</p><table><tr><th>Reset your Password by entering the below verification code manually in the application</th><th>";
 $message.=$id;
 $message.="</th></tr><tr></tr></table></body></html>";
@@ -26,6 +41,8 @@ $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 $headers .= 'From: <Online chat Team>' . "\r\n";
 			mail($email, "Password Reset", $message,$headers);
 			$response=array("Result"=>0);
+}
+ 
 }
 else
 {
