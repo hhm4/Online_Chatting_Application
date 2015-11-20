f08fe3ef5ad598a3002d1c6f85edc826066dfd83
hhm4 <hhm4@njit.edu>
2015-11-19 17:21:34 -0800

    dddfff

--- AddContact.php
+++ AddContact.php
@@ -10,30 +10,38 @@ $dbCon=mysql_select_db($config['database'], $con);
 
 $email=$_POST['EmailId'];
 $ContactsName=$_POST['ContactName'];
-
 $query="Select * From USERS where EmailId='$email'";
- $registration=mysql_query($query,$con);
+$registration=mysql_query($query,$con);
+$count=mysql_num_rows($registration);
+if($count!=0)
+{
+$row = mysql_fetch_array($registration, MYSQL_ASSOC);
+$userid=$row['UserId'];
+$userstatus=$row['UserStatus'];
+$contactsquery="Select * From CONTACTS where Contacts_UserId='$userid'";
+$contactscheck=mysql_query($contactsquery,$con);
+$contactscount=mysql_num_rows($contactscheck);
+  if($contactscount==0)
+   {
+	$sql=mysql_query("Insert into CONTACTS(Contacts_UserId,Contact_UserName,Contact_Status) values('{$userid}','{$ContactsName}','{$userstatus}')", $con);
+	$response=array("Result"=>0);
+   }
+   
+   else
+   {
+	$response=array("Result"=>1);
+   }
+	
+}
 
- $count=mysql_num_rows($registration);
- if ($count==0)
+else($count==0)
  {
 	 mail($email, "Join Online Chat", "Join online Chat www.onlineChat.com");
-	 $response=array("Result"=>0);
+	 $response=array("Result"=>2);
 	 
  }
- 
- else
- {
-	 $row = mysql_fetch_array($registration, MYSQL_ASSOC);
-	 $userid=$row['UserId'];
-	 $userstatus=$row['UserStatus'];
-	 $sql=mysql_query("Insert into CONTACTS(Contacts_UserId,Contact_UserName,Contact_Status) values('{$userid}','{$ContactsName}','{$userstatus}')", $con);
-	 $response=array("Result"=>1);
-	 
- }
-	 
- $encoded = json_encode($response);
- header('Content-type: application/json');
- echo $encoded;
+$encoded = json_encode($response);
+header('Content-type: application/json');
+echo $encoded;
 mysql_close();
 ?>
