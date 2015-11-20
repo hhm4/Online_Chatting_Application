45ea9220ab524c4544b1eaa6a52ada78c0da0ad6
mdlkumaran <mdlkumaran@gmail.com>
2015-11-20 00:28:17 -0500

    asf

--- grpchatusers.php
+++ grpchatusers.php
@@ -2,7 +2,7 @@
 
 echo "hi";
 $config=parse_ini_file("/afs/cad/u/h/h/hhm4/public_html/.mysql.ini",false,true);
-$mysqli=mysql_connect($config['host'],$config['username'],$config['password']);
+$con=mysql_connect($config['host'],$config['username'],$config['password']);
 $dbCon=mysql_select_db($config['database'], $con);
 
 $GroupImage= "someImagedir";
