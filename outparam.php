b1d80918ef783126e011b57a3a82f4175a59445c
mdlkumaran <mdlkumaran@gmail.com>
2015-11-19 19:35:17 -0500

    gsf

--- outparam.php
+++ outparam.php
@@ -8,7 +8,8 @@ $mysqli = mysqli_connect();
 $call = mysqli_prepare($mysqli, 'CALL Test_OutProcedure(?, @result)');
 mysqli_stmt_bind_param($call, 's', $username);
 mysqli_stmt_execute($call);
-
+mysqli_stmt_bind_result($call, $result);
+mysqli_stmt_fetch($call);
 echo $result;
            
 />
