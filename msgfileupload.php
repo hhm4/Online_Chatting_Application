9bc104137bb573527be1c9599d329038a83a4ef2
mdlkumaran <mdlkumaran@gmail.com>
2015-11-21 16:48:11 -0500

    gh

--- msgfileupload.php
+++ msgfileupload.php
@@ -7,11 +7,14 @@ echo "hello0";
 	$upload_dir = '/afs/cad/u/h/h/hhm4/public_html/UPLOADS/';
 //	$upload_dir_db = 'C:\\\Users\\\Kumi\\\Desktop\\\phpUpload';
 echo "hello1";
+print_r($_FILES);
 	if(is_uploaded_file($_FILES['userfile']['tmp_name']))
 	{
 		echo "hello2";
 		$dest = $_FILES['userfile']['name'];
+		print_r($_FILES);
 		echo $dest;
+		echo $upload_dir/$dest;
 //		$dest_db = "\\\\".$dest;
 //		$store_dir = $upload_dir_db.$dest_db;
 //		echo "$store_dir";
