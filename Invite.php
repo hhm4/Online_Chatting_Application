1c915c75cdb068501c8eac86a52b3e16095e6f57
hhm4 <hhm4@njit.edu>
2015-11-20 18:39:26 -0800

    kjkjkj

--- Invite.php
+++ Invite.php
@@ -11,7 +11,19 @@ $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
 // More headers
 $headers .= 'From: <Online chat Team>' . "\r\n";
-
-
-	 mail($email, "Join Online Chat",$message,$headers);
+$check= mail($email, "Join Online Chat",$message,$headers);
+if($check)
+{
+$response=array("Result"=>0);
+}
+else
+{
+	$response=array("Result"=>1);
+	
+}
+$encoded = json_encode($response);
+header('Content-type: application/json');
+echo $encoded;
+mysql_close();
+ 
 ?>
