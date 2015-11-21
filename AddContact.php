9f661d93fc53721b596b7f8518285f0c5f68bd67
hhm4 <hhm4@njit.edu>
2015-11-20 17:47:44 -0800

    cdfdf

--- AddContact.php
+++ AddContact.php
@@ -37,18 +37,6 @@ $contactscount=mysql_num_rows($contactscheck);
 
 else
  {
-	 $message = "
-<html><head><title>HTML email</title></head><body><p> Join our Exciting online Chat Application</p><table><tr><th>Please click the below link to join our exciting Chat application</th><th>www.onlinechat.com</th></tr><tr></tr>
-</table></body></html>
-";
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
-// More headers
-$headers .= 'From: <Online chat Team>' . "\r\n";
-
-
-	 mail($email, "Join Online Chat",$message,$headers);
 	 $response=array("Result"=>2);
 	 
  }
