fdfac53f8149b06173231cb205ca4e517d3c9089
hhm4 <hhm4@njit.edu>
2015-11-20 17:37:47 -0800

    jhjhjh

--- DeleteContact.php
+++ DeleteContact.php
@@ -9,37 +9,19 @@ if(!$con)
 $dbCon=mysql_select_db($config['database'], $con);
 
 $contactid=$_POST['ContactId'];
-$fromid=$_POST['FromId'];
 $query="Select * From CONTACTS where ContactId='$contactid'";
 $check=mysql_query($query,$con);
 $count=mysql_num_rows($check);
 if($count!=0)
 {
-$row = mysql_fetch_array($check, MYSQL_ASSOC);
-$userid=$row['Contacts_UserId'];
-$UserIds1=';'.$fromid.';'.$userid.';';
-$UserIds2= ';'.$userid.';'.$fromid.';';
-$contactquery="Select ChatRoomId From CHATROOM_USERS where UserIds ='$UserIds1' or UserIds ='$UserIds2' ";
-if(mysql_num_rows($contactquery)>0)
-{
-	
-$deletechatmsg=mysql_query("Delete FROM CHATMESSAGES where ChatRoomId='$contactquery'",$con);
-$deletechatroom=mysql_query("Delete FROM CHATROOM_USERS where ChatRoomId='$contactquery'",$con);
-if(mysql_affected_rows($deletechatmsg)>0 && mysql_affected_rows($deletechatroom)>0)
-{
+
 $removedata=mysql_query("Delete FROM CONTACTS where ContactId='$contactid'",$con);
-
-}
 $response=array("Result"=>0);	
-	
-}
-
-
 }
 
 else
  {
-	 $response=array("Result"=>2);
+	 $response=array("Result"=>1);
 	 
  }
 $encoded = json_encode($response);
