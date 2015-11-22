fce53b631621b64dee499cfa9807e6b7cd08e729
hhm4 <hhm4@njit.edu>
2015-11-22 15:54:43 -0800

    sdsdd

--- Verification.php
+++ Verification.php
@@ -24,10 +24,10 @@ $query="Select * From UNVERIFIED_USERS where Token=$token AND EmailId='$email'";
 	$row = mysql_fetch_array($registration, MYSQL_ASSOC);
 	 $name=$row['UserName'];
 	 $pass=$row['User_Password'];
-	 $sql=mysql_query("Insert into USERS(UserName,EmailId,User_Password) values('{$name}','{$email}','{$pass}')", $con);
-	 $delete=mysql_query("Delete FROM UNVERIFIED_USERS where EmailId='$email'",$con);
-	 if($sql)
-	 {
+	 #$sql=mysql_query("Insert into USERS(UserName,EmailId,User_Password) values('{$name}','{$email}','{$pass}')", $con);
+	 #$delete=mysql_query("Delete FROM UNVERIFIED_USERS where EmailId='$email'",$con);
+	/*  if($sql)
+	 { */
 	 $maxid=mysql_query("Select max(UserId) from USERS", $con);
 	 $row = mysql_fetch_array($registration, MYSQL_ASSOC);
 	 $max=$row['max(UserId)'];
@@ -37,7 +37,7 @@ $query="Select * From UNVERIFIED_USERS where Token=$token AND EmailId='$email'";
      $rows['UserInfo'][]=$r;
 	 $response=$rows;
    }
-	 }
+	 // }
 	 
  }
 
