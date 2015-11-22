3177c849707b9dcd574361288cc63d48f24c949c
hhm4 <hhm4@njit.edu>
2015-11-21 20:37:52 -0800

    n nn

---
+++ ChatMessage_Push.php
@@ -0,0 +1,37 @@
+<?php
+
+$config=parse_ini_file("/afs/cad/u/h/h/hhm4/public_html/.mysql.ini",false,true);
+$con=mysql_connect($config['host'],$config['username'],$config['password']);
+if(!$con)
+{
+	print "Not connected";
+}
+$dbCon=mysql_select_db($config['database'], $con);
+
+$istextmsg=$_POST[IsTextMessage];
+$message=$_POST[Message];
+$fromuserid=$_POST[FromUserId];
+$chatroomid=$_POST[ChatRoomId];
+$istextmsg = $istextmsg === 'true'? true: false;
+
+if($istextmsg)
+{
+
+   $query=mysql_query("Insert into CHATMESSAGES(ChatRoomId,FromUserId,Message) values('{$chatroomid}','{$fromuserid}','{$message}')", $con);
+   $result=mysql_query($query,$con);
+   $response = $result ? array("Result"=>0):array("Result"=>2);
+
+}
+
+else
+{
+	
+	
+}
+
+$encoded = json_encode($response);
+header('Content-type: application/json');
+echo $encoded;
+mysql_close();
+?>
+
