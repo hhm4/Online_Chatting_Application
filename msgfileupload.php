f68fcdd65d73f0ef01879e6ba4584c1adeafc81a
mdlkumaran <mdlkumaran@gmail.com>
2015-11-22 20:02:25 -0500

    dfd

--- msgfileupload.php
+++ msgfileupload.php
@@ -7,7 +7,7 @@ function upload_file()
 	$FromUserId = $_POST['FromUserId'];
 //	$upload_dir = 'C:\Users\Kumi\Desktop\phpUpload';
 echo ['FromUserId'] . $ FromUserId;
-echo "hello0";
+/*echo "hello0";
 	$upload_dir = '/afs/cad/u/h/h/hhm4/public_html/UPLOADS/';
 //	$upload_dir_db = 'C:\\\Users\\\Kumi\\\Desktop\\\phpUpload';
 echo "hello1";
@@ -39,5 +39,5 @@ print_r($_FILES);
 		echo "filename '".$_FILES['userfile']['tmp_name'] ."'.";
 		print_r($_FILES);
 	}
-}
+}*/
 ?>
