d09b63797a09acb0ac87a8c9d5d58a91b90c1e5e
mdlkumaran <mdlkumaran@gmail.com>
2015-11-22 20:49:50 -0500

    sfd

--- msgfileupload.php
+++ msgfileupload.php
@@ -1,17 +1,19 @@
+
+
 <?php
 echo "hi";
-//upload_file();
+/*upload_file();
 
-//function upload_file()
-//{
+function upload_file()
+{
 	$FromUserId = $_POST['FromUserId'];
-//	$upload_dir = 'C:\Users\Kumi\Desktop\phpUpload';
-echo $ FromUserId;
-/*echo "hello0";
+	$upload_dir = 'C:\Users\Kumi\Desktop\phpUpload';
+	echo $ FromUserId;
+	echo "hello0";
 	$upload_dir = '/afs/cad/u/h/h/hhm4/public_html/UPLOADS/';
-//	$upload_dir_db = 'C:\\\Users\\\Kumi\\\Desktop\\\phpUpload';
-echo "hello1";
-print_r($_FILES);
+	$upload_dir_db = 'C:\\\Users\\\Kumi\\\Desktop\\\phpUpload';
+	echo "hello1";
+	print_r($_FILES);
 	if(is_uploaded_file($_FILES['userfile']['tmp_name']))
 	{
 		echo "hello2";
@@ -19,9 +21,9 @@ print_r($_FILES);
 		print_r($_FILES);
 		echo $dest;
 		echo $upload_dir/$dest;
-//		$dest_db = "\\\\".$dest;
-//		$store_dir = $upload_dir_db.$dest_db;
-//		echo "$store_dir";
+		$dest_db = "\\\\".$dest;
+		$store_dir = $upload_dir_db.$dest_db;
+		echo "$store_dir";
 		$moveBool = false;
 		$moveBool = move_uploaded_file($_FILES['userfile']
 
@@ -38,6 +40,6 @@ print_r($_FILES);
 		echo "Possible file upload attack: ";
 		echo "filename '".$_FILES['userfile']['tmp_name'] ."'.";
 		print_r($_FILES);
-	}*/
-//}
+	}
+}*/
 ?>
