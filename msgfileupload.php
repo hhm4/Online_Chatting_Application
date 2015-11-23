50d304e35955848eba25092739d9014475587db6
mdlkumaran <mdlkumaran@gmail.com>
2015-11-22 20:55:25 -0500

    sdasa

--- msgfileupload.php
+++ msgfileupload.php
@@ -6,9 +6,10 @@ upload_file();
 
 function upload_file()
 {
-	$FromUserId = $_POST['FromUserId'];
-	$upload_dir = 'C:\Users\Kumi\Desktop\phpUpload';
-	echo $ FromUserId;
+	echo "hi";
+//	$FromUserId = $_POST['FromUserId'];
+//	$upload_dir = 'C:\Users\Kumi\Desktop\phpUpload';
+//	echo $ FromUserId;
 /*	echo "hello0";
 	$upload_dir = '/afs/cad/u/h/h/hhm4/public_html/UPLOADS/';
 	$upload_dir_db = 'C:\\\Users\\\Kumi\\\Desktop\\\phpUpload';
