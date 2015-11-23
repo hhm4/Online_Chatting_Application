eec16ea0c732a38eaabfd2f0b08def36e793e46b
hhm4 <hhm4@njit.edu>
2015-11-22 23:08:27 -0800

    dfdfdfd

--- SignIn.php
+++ SignIn.php
@@ -1,28 +1,18 @@
-	<?php
-
-
+<?php
 $config=parse_ini_file("/afs/cad/u/h/h/hhm4/public_html/.mysql.ini",false,true);
 $con=mysql_connect($config['host'],$config['username'],$config['password']);
 
 $dbcon=mysql_select_db($config['database'],$con);
 $email=$_POST['EmailId'];
 $password=$_POST['Password'];
-
-$query = sprintf("SELECT EmailId,User_Password FROM USERS
-    WHERE EmailId='%s' AND User_Password='%s'",
-    mysql_real_escape_string($email),
-    mysql_real_escape_string($password));
-#$authentication=mysql_query($query,$con);
 $query="select * from USERS where EmailId='$email' AND User_Password='$password'";
 $authentication=mysql_query($query,$con);
-
 $count=mysql_num_rows($authentication);
 
 if ($count>0){
 	
   while($r = mysql_fetch_assoc($authentication))
   {
-	  echo "1";
 	 $userid=$r['UserId'];
 	 echo $userid;
    }
