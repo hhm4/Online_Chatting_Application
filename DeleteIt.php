661b595dacb954c0fbec536f0b2f642f7121952d
hhm4 <hhm4@njit.edu>
2015-11-27 03:02:55 -0800

    xzxzx

--- DeleteIt.php
+++ DeleteIt.php
@@ -2,6 +2,13 @@
 // Example 1
 $pizza  = "piece1;piece2;piece3;piece4;piece5;piece6";
 $pieces = explode(";", $pizza);
+
+foreach ($pieces as $p)
+{
+	
+	echo "Current Value ".$p;
+	
+}
 echo count($pieces);
 echo $pieces[0]; // piece1
 echo $pieces[1]; // piece2
