8bbd50f8dc5cdd78bf063c3558e4090f8e1622a6
hhm4 <hhm4@njit.edu>
2015-11-26 22:58:03 -0500

    removed condition

--- SignIn.php
+++ SignIn.php
@@ -17,7 +17,7 @@ if ($count>0){
 	 $userinfo =$r1;
   }
    
-$query1="Select * From CONTACTS where CONTACTS_FROMUSERID ='$userid' AND IsAContact=1";
+$query1="Select * From CONTACTS where CONTACTS_FROMUSERID ='$userid'";
 $newvalues=mysql_query($query1,$con);
   while($r = mysql_fetch_assoc($newvalues))
   {
